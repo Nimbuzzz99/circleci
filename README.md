ea8c2f51a2976a66b38729a248bd46ae1f8f0066
Nimbuzzz99 <sharoon8105@gmail.com>
2018-07-17 11:23:12 +0530

    second circleci

---
+++ README.md
@@ -0,0 +1,1 @@
+[![CircleCI](https://circleci.com/gh/Nimbuzzz99/circleci.svg?style=svg)](https://circleci.com/gh/Nimbuzzz99/circleci)
---
+++ src/test/java/com/stackroute/movieapp/repository/MovieAppRepositoryTestIT.java
@@ -0,0 +1,16 @@
+package com.stackroute.movieapp.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+public class MovieAppRepositoryTestIT {
+
+    @Test
+    public void fakeTest(){
+        assertTrue(true);
+    }
+}
